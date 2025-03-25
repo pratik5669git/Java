1a94d0c7ede843737be73b8873271e7a83ef65ba
Pratik kumar <148235773+pratik5669git@users.noreply.github.com>
2025-03-25 23:35:00 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();//only takes input first word not after the space
+        // System.out.println(input);
+
+        // String name = sc.nextLine();// takes the whole line as input
+        // System.out.println(name);
+
+        // int n = sc.nextInt();//takes the integer
+        // System.out.println(n);
+
+        // float price = sc.nextFloat();//takes the float values
+        // System.out.println(price);
+
+        // boolean a = sc.nextBoolean();//take either true or false
+        // System.out.println(a);
+
+        // short b = sc.nextShort();//takes the short value
+        // System.out.println(b);
+
+        long h = sc.nextLong();
+        System.out.println(h);
+
+
+
+    }
+}
