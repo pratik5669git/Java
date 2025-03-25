c0b331b8868c7ad8f62664af6ccdad0051dd1ca1
Pratik kumar <148235773+pratik5669git@users.noreply.github.com>
2025-03-25 23:35:12 +0530

    Add files via upload

---
+++ data_types.java
@@ -0,0 +1,16 @@
+public class data_types {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+        short n = 240;
+        System.out.println(n);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean m = true;
+        System.out.println(m);
+        int j = 23;
+        System.out.println(j);
+        //long
+        //double
+    }
+}
