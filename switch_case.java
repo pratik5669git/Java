1bf6a4ee5363ea064176bed23a4c5280de0d65f0
Pratik kumar <148235773+pratik5669git@users.noreply.github.com>
2025-03-29 23:33:00 +0530

    Add files via upload

---
+++ switch_case.java
@@ -0,0 +1,16 @@
+public class switch_case{
+    public static void main(String[] args) {
+        int num=3;
+        switch (num) {
+            case 1:
+                System.out.println("hi");
+                break;
+            case 3:
+                System.out.println("hello");
+                break;
+            default:
+                System.out.println("wrong choice");
+                break;
+        }
+    }
+}
