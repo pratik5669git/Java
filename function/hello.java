0c0be81c1468300bd552f1076436a105384486d8
Pratik kumar <148235773+pratik5669git@users.noreply.github.com>
2025-04-08 08:38:47 +0530

    Add files via upload

---
+++ function/hello.java
@@ -0,0 +1,10 @@
+public class hello {
+   public static void printing(){
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+   } 
+   public static void main(String[] args) {
+    printing();//function call
+   }
+}
