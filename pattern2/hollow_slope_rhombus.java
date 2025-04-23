f8373d038f8193a4d66bd5067f638f7bd5fdd0c5
Pratik kumar <148235773+pratik5669git@users.noreply.github.com>
2025-04-23 20:32:24 +0530

    Add files via upload

---
+++ pattern2/hollow_slope_rhombus.java
@@ -0,0 +1,15 @@
+public class hollow_slope_rhombus {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=9;j++){
+                if(i==1 && j>=5 || i==5 && j<=5 || i+j==10 || i+j==6){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
